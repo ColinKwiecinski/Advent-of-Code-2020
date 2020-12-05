6d1a3f8e347eb4ac384e0f2a227a109845825e95
colinkwiecinski <colinkwiecinski@gmail.com>
2020-12-05 11:13:31 -0500

    initial commit d5 pt2

--- src/Day5.java
+++ src/Day5.java
@@ -1,18 +1,30 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Day5 {
     private static final String INPUT_FILE = "input_day5.txt";
 
     public static void main(String[] args) {
-        System.out.println(findSeat());
+        System.out.println("Highest Seat ID: " + findSeat());
     }
 
     private static int findSeat() {
         Scanner input = new FileReader(INPUT_FILE).getScanner();
+        ArrayList<Integer> seatIDs = new ArrayList<>();
         int max = 0;
         while (input.hasNextLine()) {
-            max = Math.max(max, getRow(input.nextLine(), 0, 127));
+            int nextId = getRow(input.nextLine(), 0, 127);
+            seatIDs.add(nextId);
+            max = Math.max(max, nextId);
         }
+
+        // Find missing seat using n*(n+1)/2 sum logic
+        Collections.sort(seatIDs); // More runtime intensive but simpler to code
+        int sum = 0;
+        for (int i : seatIDs) sum += i;
+        int missing = (max * (max + 1) / 2) - sum - ((seatIDs.get(0) - 1) * seatIDs.get(0) / 2);
+        System.out.println("Missing Seat: " + missing);
         return max;
     }
 
@@ -41,5 +53,4 @@ public class Day5 {
         }
         return -1;
     }
-
 }
