630e3d5a7098df3def4b3a35649072eae93183e7
colinkwiecinski <colinkwiecinski@gmail.com>
2020-12-14 13:18:16 -0500

    day 10 part 2

--- src/Day10.java
+++ src/Day10.java
@@ -1,9 +1,8 @@
-import com.sun.javafx.geom.Edge;
-
 import java.util.*;
 
 public class Day10 {
     private static final String INPUT_FILE = "in/input_day10.txt";
+    private static final int TRIB_SIZE = 5;
 
     public static void main(String[] args) {
         Scanner input = new FileReader(INPUT_FILE).getScanner();
@@ -11,14 +10,16 @@ public class Day10 {
         while (input.hasNextInt()) {
             inputData.add(input.nextInt());
         }
+        inputData.add(0); // Wall plug has joltage zero
         Collections.sort(inputData);
+        inputData.add(inputData.get(inputData.size() - 1) + 3); // Phone adapter has + 3 to max
         System.out.println(partOne(inputData));
         System.out.println(partTwo(inputData));
     }
 
     private static int partOne(ArrayList<Integer> input) {
         int ones = 0;
-        int threes = 1; // start at one because of built in phone adapter
+        int threes = 0; // start at one because of built in phone adapter
         int joltSum = 0;
         for (int i = 0; i < input.size(); i++) {
             int diff = input.get(i) - joltSum;
@@ -32,71 +33,39 @@ public class Day10 {
         return ones * threes;
     }
 
-    private static int partTwo(ArrayList<Integer> input) {
-        int sum = 0;
+    private static long partTwo(ArrayList<Integer> input) {
+        long sum = 1;
         ArrayList<Integer> splitIndices = new ArrayList<>();
+        splitIndices.add(0);
         for (int i = 0; i < input.size() - 1; i++) {
             int diff = input.get(i + 1) - input.get(i);
             if (diff == 3) {
-                splitIndices.add(i);
+                splitIndices.add(i + 1);
             }
         }
+        ArrayList<Integer> tribSeq = buildTrib(1, 1, 2, TRIB_SIZE);
         for (int i = 0; i < splitIndices.size() - 1; i++) {
-            sum *= partTwoSolver(input.subList(splitIndices.get(i), splitIndices.get(i + 1)));
+            sum *= partTwoTrib(input.subList(splitIndices.get(i), splitIndices.get(i + 1)), tribSeq);
         }
         return sum;
     }
 
-    private static int partTwoSolver(List<Integer> input) {
-        // break into subsections at each three, find number of permutations for each subsection, multiply together to get result.
-        Map<Vertex, Edge> edgeTo = new HashMap<>();
-        Map<Vertex, Integer> distTo = new HashMap<>();
-        // init a set for visited nodes?
-        // init a queue for order to visit nodes?
-
-        for (Integer i : input) {
-            // create every vertex based on input
-            // set.add(new Vertex(i));
-        }
-
-        // add edges between vertices based on input size. keep track of sum? don't add edges that are more than 3 apart
-
-        // build path tree
-
-        // backtrack to find number of paths...
-
-        // return count of paths between s and t. hopefully shouldn't need to verify that they're not more than three apart
-        return -1;
+    private static int partTwoTrib(List<Integer> input, ArrayList<Integer> tribs) {
+        return tribs.get(input.size() - 1);
     }
 
-
-    private class Vertex {
-        private final int data;
-
-        public Vertex(int data) {
-            this.data = data;
+    // Returns a Tribonacci sequence with given seeds a, b, c, up to specified size
+    private static ArrayList<Integer> buildTrib(int a, int b, int c, int size) {
+        int count = 2; // first three seed numbers are included
+        ArrayList<Integer> result = new ArrayList<>();
+        result.add(a);
+        result.add(b);
+        result.add(c);
+        while (count < size) {
+            int nextSum = result.get(count) + result.get(count - 1) + result.get(count - 2);
+            result.add(nextSum);
+            count++;
         }
-
-        public int getData() {
-            return data;
-        }
-    }
-
-    private class Edge {
-        private final Vertex from;
-        private final Vertex to;
-
-        public Edge(Vertex to, Vertex from) {
-            this.to = to;
-            this.from = from;
-        }
-
-        public Vertex from() {
-            return this.from;
-        }
-
-        public Vertex to() {
-            return this.to;
-        }
+        return result;
     }
 }
